8e18928f0be52764c94f5fd5e4729c826a7e5fab
Aurora <eunbipower@gmail.com>
2023-07-26 15:23:19 +0900

    style:좋아요 버튼메인 적용. 공통으로 이동

--- src/components/main/RoomCard.jsx
+++ src/components/main/RoomCard.jsx
@@ -1,68 +1,20 @@
 import React from "react";
 import * as S from "../../style/Main/MainPage";
+import LikeButton from "../../components/common/LikeButton/LikeButton"
 
 function RoomCard() {
     return (
         <S.RoomCards>
             <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
-                <h3>주소</h3>
-                <p>타이틀</p>
-                <p>가격/박</p>
-            </S.RoomCard>
-            <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
-                <h3>주소</h3>
-                <p>타이틀</p>
-                <p>가격/박</p>
-            </S.RoomCard>
-            <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
-                <h3>주소</h3>
-                <p>타이틀</p>
-                <p>가격/박</p>
-            </S.RoomCard>
-            <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
-                <h3>주소</h3>
-                <p>타이틀</p>
-                <p>가격/박</p>
-            </S.RoomCard>
-            <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
-                <h3>주소</h3>
-                <p>타이틀</p>
-                <p>가격/박</p>
-            </S.RoomCard>
-            <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
-                <h3>주소</h3>
-                <p>타이틀</p>
-                <p>가격/박</p>
-            </S.RoomCard>
-            <S.RoomCard>
-                <img
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
-                    alt="장소"
-                />
+                <S.RoomsImage>
+                    <img
+                        src="https://a0.muscache.com/im/pictures/miso/Hosting-577765450265818795/original/69d05e75-fba6-4b31-bfa1-f00264e5766c.jpeg?im_w=720"
+                        alt="장소"
+                    />
+                    <span>
+                        <LikeButton />
+                    </span>
+                </S.RoomsImage>
                 <h3>주소</h3>
                 <p>타이틀</p>
                 <p>가격/박</p>
@@ -70,5 +22,4 @@ function RoomCard() {
         </S.RoomCards>
     );
 }
-
 export default RoomCard;
